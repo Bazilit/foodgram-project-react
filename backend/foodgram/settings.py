731a0f11b2cfa661af9761e0eaf4ecba5b0ba728
Bazilit <vip.shark.black@gmail.com>
2022-06-17 03:49:15 +0300

    server

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -2,6 +2,8 @@ import os
 
 from dotenv import load_dotenv
 
+load_dotenv()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='acej2l)w5t8dyl+vaep9#ywqja3$ai7(!9@*m@diw+geb@22%*')
@@ -69,12 +71,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 
 DATABASES = {
-    'DEFAULT': {
-        'ENGINE': os.getenv('DB_ENGINE', default='DJANGO.DB.BACKENDS.POSTGRESQL'),
-        'NAME': os.getenv('DB_NAME', default='POSTGRES'),
-        'USER': os.getenv('POSTGRES_USER', default='POSTGRES'),
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='452882'),
-        'HOST': os.getenv('DB_HOST', default='DB'),
+        'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
